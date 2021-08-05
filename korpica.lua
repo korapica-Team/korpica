d0665f994e42b899db587067b0679e08ff85cf60
korapica-Team <67028232+korapica-Team@users.noreply.github.com>
2021-08-05 23:14:30 +0300

    Update korpica.lua

--- korpica.lua
+++ korpica.lua
@@ -8486,7 +8486,7 @@ if text == 'السورس' or text == 'سورس' or text == 'ياسورس' or tex
 local url,res = https.request('http://shahum.ml/subscrib-source/SouRceSHaHuM/ashtrak.php?id='..msg.sender_user_id_)
 data = JSON.decode(url)
 if data.Ch_Member.info ~= true then
-send(msg.chat_id_,msg.id_,'- اشترك في قناة السورس أولا\n[❲ [SHaHuM source .](t.me/SouRceSHaHuM) ] ❳')
+send(msg.chat_id_,msg.id_,'- اشترك في قناة السورس أولا\n[❲ @SouRceSHaHuM ] ❳')
 return false
 end
 local Text =[[
